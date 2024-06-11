f7beb6a4c23041ff020e395d48967d6831a71d48
Jeong-ki <7863z@naver.com>
2024-06-11 23:03:35 +0900

    update: color details

--- src/color.js
+++ src/color.js
@@ -1,15 +1,15 @@
-export function colorize(text, color) {
-  const colorCodes = {
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    cyan: "\x1b[36m",
-    white: "\x1b[37m",
-    reset: "\x1b[0m",
-    bold: "\x1b[1m",
-  };
+const colorCodes = {
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  reset: "\x1b[0m",
+  bold: "\x1b[1m",
+};
 
+export function colorize(text, color) {
   return `${colorCodes[color]}${text}${colorCodes.reset}`;
 }
